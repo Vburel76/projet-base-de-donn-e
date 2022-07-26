848ddb5a978dd52e0c29c52b86dc62808501d00f
Vburel76 <valentin.burel.76600@gmail.com>
2022-07-26 14:15:28 +0200

    img

--- views/home.php
+++ views/home.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -7,12 +8,26 @@
     <title>TravelCorp</title>
     <link rel="stylesheet" href="../public/style/style.css">
 </head>
+
 <body>
+    <h1>Travel Corp</h1>
 
-<h1>Travel Corp</h1>
+    <nav>
+        <div class="navPosition">
+            <ul class="navPosition">
+                <a>Amérique</a>
+                <a>Europe</a>
+                <a>Asie</a>
+                <a>Océanie</a>
+                <a>Afrique</a>
+            </ul>
+        </div>
+    </nav>
 
 
 
-    
+
+
 </body>
+
 </html>
